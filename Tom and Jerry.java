034c02088bb04d5ae3853a5e61ffe11a2cf3fed4
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-09-29 19:53:34 +0530

    Update Tom and Jerry.java

--- Tom and Jerry.java
+++ Tom and Jerry.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    static int numsGame(int N) {
+        // code here
+            if(N % 2 == 0){
+                return 1;
+            }
+            else
+                return 0;
+    }
+}
