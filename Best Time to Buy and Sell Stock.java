d560e374ffa6f90cbf9128437d4642a1cecee185
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-10-12 01:01:29 +0530

    Update Best Time to Buy and Sell Stock.java

--- Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock.java
@@ -18,3 +18,18 @@ class Solution {
         return maxProfit;
     }
 }
+
+// O(n) and O(1) space 
+
+class Solution {
+    public int maxProfit(int[] prices) {
+       int maxProfit = 0;
+       int minSofar = prices[0];
+        for(int i=0;i<prices.length;i++){
+            minSofar = Math.min(minSofar , prices[i]);
+            int profit = prices[i] - minSofar;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+        return maxProfit;
+    }
+}
