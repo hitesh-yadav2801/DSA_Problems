2ada6daf9c9f6c6bab1a7a9634e682ebd52b88a8
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-10-19 23:15:26 +0530

    Update Intersection of Two Arrays II.java

--- Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II.java
@@ -1,3 +1,5 @@
+// using HashMap
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -18,6 +20,31 @@ class Solution {
         int[] ans = new int[list.size()];
         for(int i=0;i<list.size();i++){
             ans[i] = list.get(i);
+        }
+        return ans;
+    }
+}
+
+
+// using Hashset
+
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            set2.add(nums2[i]);
+        }
+        set2.retainAll(set1);
+        
+        int[] ans = new int[set2.size()];
+        int i=0;
+        for(int e : set2){
+             ans[i++] = e;
         }
         return ans;
     }
