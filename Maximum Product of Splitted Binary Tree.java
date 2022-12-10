64e7909e1b471300ed444556f45b769528d86db7
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-12-10 23:21:47 +0530

    Update Maximum Product of Splitted Binary Tree.java

--- Maximum Product of Splitted Binary Tree.java
+++ Maximum Product of Splitted Binary Tree.java
@@ -39,3 +39,47 @@ class Solution {
         return l + r + root.val;
     }
 }
+
+
+// Optimized method using recursion(to calculate subsum)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long m = 1000000007;
+    long maxRes = Long.MIN_VALUE;
+    public int maxProduct(TreeNode root) {
+        long sum = totalSum(root); 
+        helper(root, sum);      
+        return (int)(maxRes % m);
+    }
+    public long helper(TreeNode root, long sum){
+        if(root == null) return 0;
+        long subsum = helper(root.left, sum) + 
+        helper(root.right, sum) + root.val;
+        long res = (sum - subsum) * subsum;
+        maxRes = Math.max(res, maxRes);
+        return subsum;
+    }
+
+    public long totalSum(TreeNode root){
+        if(root == null) return 0;
+        long l = totalSum(root.left);
+        long r = totalSum(root.right);
+        return l + r + root.val;
+    }
+}
