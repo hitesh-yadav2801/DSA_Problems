7785eefefb2ef82ef308c08f6806703247abb631
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-12-12 19:49:15 +0530

    Update Climbing Stairs.java

--- Climbing Stairs.java
+++ Climbing Stairs.java
@@ -10,4 +10,17 @@ class Solution {
     }
 }
 
-//
+// using dp
+
+class Solution {
+    public int climbStairs(int n) {
+      if(n==1) return 1;
+      int[] dp = new int[n];
+      dp[n-1] = 1;
+      dp[n-2] = 2;
+      for(int i=n-3;i>=0;i--){
+          dp[i] = dp[i+1] + dp[i+2];
+      }
+      return dp[0];
+    }
+}
