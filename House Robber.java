381477cb9fc609037526642fd9683fee22a39da7
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-12-14 23:08:00 +0530

    Update House Robber.java

--- House Robber.java
+++ House Robber.java
@@ -15,3 +15,20 @@ class Solution {
 
 // Using DP
 
+class Solution {
+    Integer[] dp;
+    public int rob(int[] nums) {
+        dp = new Integer[nums.length];
+        return robFrom(nums, 0);  
+    }
+    public int robFrom(int[] nums, int curIndex){
+        if(curIndex >= nums.length) return 0;
+        if(dp[curIndex] != null) return dp[curIndex];
+
+        int rob = nums[curIndex] + robFrom(nums, curIndex + 2);
+        int notRob = robFrom(nums, curIndex + 1);
+
+        return dp[curIndex] = Math.max(rob, notRob);
+    }
+}
+
