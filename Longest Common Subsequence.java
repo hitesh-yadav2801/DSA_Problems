9de11e4241c0087b65f0b6cc0bb8a53704653d86
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-12-15 22:08:08 +0530

    Update Longest Common Subsequence.java

--- Longest Common Subsequence.java
+++ Longest Common Subsequence.java
@@ -1,3 +1,5 @@
+// using recursion(Gives TLE)
+
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         return lcs(text1, text2, 0, 0);
@@ -12,5 +14,26 @@ class Solution {
             return Math.max(lcs(s1, s2, n1+1, n2), 
             lcs(s1, s2, n1, n2+1));
         }
+    }
+}
+
+// Using DP
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int dp[][] = new int[n1+1][n2+1];
+
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 }
