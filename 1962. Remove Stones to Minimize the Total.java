66475b683a35c823670ba07c135f99398f3ee747
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2022-12-28 21:35:34 +0530

    Update 1962. Remove Stones to Minimize the Total.java

--- 1962. Remove Stones to Minimize the Total.java
+++ 1962. Remove Stones to Minimize the Total.java
@@ -17,3 +17,25 @@ class Solution {
         return minSum;
     }
 }
+
+
+// Little bit optimized
+
+class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = piles.length;
+        int minSum = 0;
+        for(int i=0;i<n;i++){
+            pq.add(piles[i]);
+            minSum += piles[i];
+        }
+        while(pq.size() > 0 && k-->0){
+            int temp = pq.remove();
+            int dummy = temp - (int)Math.floor(temp/2);
+            minSum -= (int)Math.floor(temp/2);
+            pq.add(dummy);
+        }
+        return minSum;
+    }
+}
