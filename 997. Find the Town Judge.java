8c1537d65d4a80406f7f9b39e65f2c7c73c86641
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-01-23 14:38:09 +0530

    Update 997. Find the Town Judge.java

--- 997. Find the Town Judge.java
+++ 997. Find the Town Judge.java
@@ -16,3 +16,21 @@ class Solution {
         return -1;
     }
 }
+
+// Another method
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] count = new int[n+1];
+        for(int i=0;i<trust.length;i++){
+            count[trust[i][0]]--;
+            count[trust[i][1]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(count[i] == n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
