a6bcd9a400e775ddb520c3d7a0f9d01949c10a39
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-02-03 23:08:32 +0530

    Update 6. Zigzag Conversion.java

--- 6. Zigzag Conversion.java
+++ 6. Zigzag Conversion.java
@@ -22,3 +22,35 @@ class Solution {
         return sb.toString();
     }
 }
+
+// optimized Approach
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows <= 1){
+            return s;
+        }
+        ArrayList<ArrayList<Character>> list = new ArrayList<>();
+        for(int i = 0 ; i < numRows; i++){
+            list.add(new ArrayList<>());
+        }
+        int dir = 1;
+        int curRow = 0;
+        for(char ch : s.toCharArray()){
+            list.get(curRow).add(ch);
+            if(curRow == numRows -1){
+                dir = -1;
+            } else if(curRow == 0){
+                dir = 1;
+            }
+            curRow += dir;
+        }
+        StringBuilder sb = new StringBuilder(); 
+        for(int i=0;i<list.size();i++){
+            for(char ch : list.get(i)){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
