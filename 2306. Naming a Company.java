b74ef6503b37eb61a35f64078b073dc1a8f0e7b4
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-02-10 00:04:56 +0530

    Update 2306. Naming a Company.java

--- 2306. Naming a Company.java
+++ 2306. Naming a Company.java
@@ -20,3 +20,35 @@ class Solution {
         return set.size() - ideas.length;
     }
 }
+
+// Optimal Solution
+
+class Solution {
+    public long distinctNames(String[] ideas) {
+        Set<String>[] set = new Set[26];
+        for(int i = 0; i < 26; i++){
+            set[i] = new HashSet<>();
+        }
+        for(String idea : ideas){
+            set[idea.charAt(0) - 'a'].add(idea.substring(1));
+        }
+        int[][] same = new int[26][26];
+        for(int i = 0; i < 26; i++){
+            for(String s : set[i]){
+                for(int j = i+1; j < 26; j++){
+                    if(set[j].contains(s)){
+                        same[i][j]++;
+                    }
+                }
+            }
+        }
+        long res = 0;
+        for(int i = 0; i < 26; i++){
+            for(int j = i+1; j < 26; j++){
+                res += (set[i].size() - same[i][j]) * 
+                (set[j].size() - same[i][j]) * 2;
+            }
+        }
+        return res;
+    }
+}
