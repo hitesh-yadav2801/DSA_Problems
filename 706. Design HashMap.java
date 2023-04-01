8b3fcb2cde8beae1d57dd998bd4613aa51fa1ed5
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-04-02 00:13:47 +0530

    Create 706. Design HashMap.java

---
+++ 706. Design HashMap.java
@@ -0,0 +1,28 @@
+class MyHashMap {
+    int[] arr;
+    int size = 1000001;
+    public MyHashMap() {
+        arr = new int[size]; 
+        Arrays.fill(arr, -1);  
+    }
+    
+    public void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    public int get(int key) {
+            return arr[key];
+    }
+    
+    public void remove(int key) {
+        arr[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
