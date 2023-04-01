700d5156ed9621a368e1983f086d1701edf471dc
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-04-01 10:24:36 +0530

    Update 704. Binary Search.java

--- 704. Binary Search.java
+++ 704. Binary Search.java
@@ -21,3 +21,20 @@ class Solution {
         }
     }
 }
+
+// Iterative solution
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        while(s < e){
+            int mid = s + (e - s) / 2;
+            if(nums[mid] == target) return mid;
+            else if (nums[mid] > target) e = mid - 1;
+            else if(nums[mid] < target) s = mid + 1;
+        }
+        if(nums[s] != target) return -1;
+        return s;
+    }
+}
