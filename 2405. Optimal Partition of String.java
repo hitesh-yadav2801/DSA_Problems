85c6cb087bd1a0762bf0e391dbdcd4f8eead14c5
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-04-05 01:13:07 +0530

    Update 2405. Optimal Partition of String.java

--- 2405. Optimal Partition of String.java
+++ 2405. Optimal Partition of String.java
@@ -1,3 +1,5 @@
+// using HashSet
+
 class Solution {
     public int partitionString(String s) {
         HashSet<Character> set = new HashSet<>();
@@ -10,6 +12,26 @@ class Solution {
                 set.add(key);
             } else {
                 set.add(key);
+            }
+        }
+        return ans;
+    }
+}
+
+
+// using 26 size array
+
+class Solution {
+    public int partitionString(String s) {
+        int ans = 1;
+        int[] key = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            if(key[s.charAt(i) - 'a'] == 1){
+                ans++;
+                Arrays.fill(key, 0);
+                key[s.charAt(i) - 'a'] = 1;
+            } else {
+                key[s.charAt(i) - 'a'] = 1;
             }
         }
         return ans;
