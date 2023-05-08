71d39b97e489021dfa7a49f9364fd7d5a6a215ff
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-05-08 14:42:42 +0530

    Update Second Largest.java

--- Second Largest.java
+++ Second Largest.java
@@ -47,3 +47,59 @@ class Solution {
         return secLargest;
     }
 }
+
+// optimal approach
+
+
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().print2largest(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int print2largest(int arr[], int n) {
+        int largest = arr[0];
+        int secLargest = -1;
+        for(int i = 1; i < n; i++){
+            if(arr[i] > largest){
+                secLargest = largest;
+                largest = arr[i];
+            } else if(arr[i] < largest && arr[i] > secLargest){
+                secLargest = arr[i];
+            }
+        }
+        return secLargest;
+    }
+}
+
