25f0a0e07056fe422fd1bd5352721216bad308a6
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-05-11 23:30:55 +0530

    Update 9. Palindrome Number.java

--- 9. Palindrome Number.java
+++ 9. Palindrome Number.java
@@ -34,3 +34,19 @@ class Solution {
         return (x==rev || x==rev/10);
     }
 }
+
+// optimal
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int num = 0;
+        int temp = x;
+        while(x > 0){
+            int rem = x % 10;
+            num = num * 10 + rem;
+            x /= 10;
+        }
+        if(temp == num) return true;
+        return false;
+    }
+}
