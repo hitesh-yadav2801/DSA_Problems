d957467dccfb5e8ad022b65ba389ae5dfcc4a805
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-05-12 23:28:59 +0530

    Update 2140. Solving Questions With Brainpower.java

--- 2140. Solving Questions With Brainpower.java
+++ 2140. Solving Questions With Brainpower.java
@@ -18,3 +18,30 @@ class Solution {
         return Math.max(taken, not_taken);
     }
 }
+
+// Using Recursion + memoisation
+
+class Solution {
+    public long mostPoints(int[][] questions) {
+        long[] dp = new long[questions.length];
+        Arrays.fill(dp, -1l);
+        long res = rec(questions, 0, dp);
+        return res;
+    }
+    private long rec(int[][] questions, int idx, long[] dp){
+        int len = questions.length;
+        if(idx >= len){
+            return 0;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+        long taken = questions[idx][0] + 
+        rec(questions, idx + questions[idx][1] + 1, dp);
+        long not_taken = rec(questions, idx + 1, dp);
+
+        dp[idx] = Math.max(taken, not_taken);
+
+        return dp[idx];
+    }
+}
