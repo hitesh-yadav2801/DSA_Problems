0b7637efd5799d741cc3ca2406f056a8e7eca817
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-05-18 13:24:14 +0530

    Update 725. Split Linked List in Parts.java

--- 725. Split Linked List in Parts.java
+++ 725. Split Linked List in Parts.java
@@ -42,3 +42,46 @@ class Solution {
         return ans;
     }
 }
+
+
+// Without using space
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] ans = new ListNode[k];
+        ListNode temp = head;
+        int n = 0;
+        while(temp != null){
+            temp = temp.next;
+            n++;
+        }
+        int len = n / k;
+        int rem = n % k;
+        
+        int idx = 0;
+        temp = head;
+        ListNode prev = null;
+        while(temp != null){
+            ans[idx] = temp;
+            for(int j = 0; j < len + (rem > 0 ? 1 : 0); j++){
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = null;
+            idx++;
+            rem--;
+        }
+        return ans;
+    }
+}
