e415663eff0065eaf8078d5115dc3496358acddb
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-05-23 11:01:41 +0530

    Update 160. Intersection of Two Linked Lists.java

--- 160. Intersection of Two Linked Lists.java
+++ 160. Intersection of Two Linked Lists.java
@@ -51,3 +51,36 @@ public class Solution {
         return temp1;
     }
 }
+
+// Using HashMap
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp = headA;
+        HashMap<ListNode, Integer> map = new HashMap<>();
+        while(temp != null){
+            map.put(temp, 1);
+            temp = temp.next;
+        }
+        temp = headB;
+        while(temp != null){
+            if(map.containsKey(temp)){
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
