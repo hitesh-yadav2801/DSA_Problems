9b87f444f0a9b63828e02819fd7a50472495d377
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-05 22:13:00 +0530

    Update 73. Set Matrix Zeroes.java

--- 73. Set Matrix Zeroes.java
+++ 73. Set Matrix Zeroes.java
@@ -1,3 +1,4 @@
+// brute force
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
@@ -22,6 +23,41 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(temp[i][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+// better solution
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(col[i] == 1){
+                for(int j = 0; j < m; j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        for(int i = 0; i < m; i++){
+            if(row[i] == 1){
+                for(int j = 0; j < n; j++){
                     matrix[i][j] = 0;
                 }
             }
