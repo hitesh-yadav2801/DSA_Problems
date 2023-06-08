caadc6773e8f768797e0ec15fe4764ba4783ed2c
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-08 10:20:49 +0530

    Update 1351. Count Negative Numbers in a Sorted Matrix.java

--- 1351. Count Negative Numbers in a Sorted Matrix.java
+++ 1351. Count Negative Numbers in a Sorted Matrix.java
@@ -1,3 +1,5 @@
+//O(n^2) solution
+
 class Solution {
     public int countNegatives(int[][] grid) {
         int m = grid.length;
@@ -10,6 +12,28 @@ class Solution {
                 } else{
                     break;
                 }
+            }
+        }
+        return count;
+    }
+}
+
+//O(m+n) solution
+
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int i = row - 1;
+        int j = 0;
+        int count = 0;
+
+        while(i >= 0 && j < col){
+            if(grid[i][j] < 0){
+                count += col - j;
+                i--;
+            } else {
+                j++;
             }
         }
         return count;
