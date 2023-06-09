b038ed90a5582789d5d82c65deadf7ef555fa10c
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-09 09:19:52 +0530

    Update 744. Find Smallest Letter Greater Than Target.java

--- 744. Find Smallest Letter Greater Than Target.java
+++ 744. Find Smallest Letter Greater Than Target.java
@@ -1,3 +1,5 @@
+// o(n)
+
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         for(int i = 0; i < letters.length; i++){
@@ -6,5 +8,25 @@ class Solution {
             }
         }
         return letters[0];
+    }
+}
+
+// O(logN) binary search
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int l = 0;
+        int r = n - 1;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if(letters[mid] <= target){
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l == n ? letters[0] : letters[l];
     }
 }
