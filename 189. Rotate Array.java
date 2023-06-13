8b86d870084b8eeebdf82a78a2e0d6e4d210ed71
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-13 11:55:43 +0530

    Update 189. Rotate Array.java

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -11,3 +11,24 @@ class Solution {
         System.arraycopy(ans, 0, nums, 0, n);
     }
 }
+
+// Without any extra space
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0 , n-k-1);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+    private void reverse(int[] nums, int i, int j){
+        while(i < j){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            i++;
+            j--;
+        }
+    }
+}
