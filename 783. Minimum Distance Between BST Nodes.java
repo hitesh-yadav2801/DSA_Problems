4046f8a8e0263a2b644355660792e59683776284
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-14 08:29:53 +0530

    Update 783. Minimum Distance Between BST Nodes.java

--- 783. Minimum Distance Between BST Nodes.java
+++ 783. Minimum Distance Between BST Nodes.java
@@ -36,3 +36,24 @@ class Solution {
         inOrder(root.right, ansList);
     }
 }
+
+// optimal solution without extra space
+
+class Solution {
+    int prev = Integer.MAX_VALUE;
+    int ans = Integer.MAX_VALUE;
+    public int minDiffInBST(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+    public void inOrder(TreeNode root){
+        if(root.left != null){
+            inOrder(root.left);
+        }
+        ans = Math.min(ans, Math.abs(root.val - prev));
+        prev = root.val;
+        if(root.right != null){
+            inOrder(root.right);
+        }
+    }
+}
