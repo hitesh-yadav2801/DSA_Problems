11df00425c435853f37b4b2c4ab61c5145f409be
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-15 10:03:51 +0530

    Update 74. Search a 2D Matrix.java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -1,3 +1,7 @@
+// brute force
+// by linear search
+
+// better solution
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
@@ -8,6 +12,31 @@ class Solution {
             if(matrix[i][j] == target) return true;
             else if(matrix[i][j] > target) i--;
             else j++;
+        }
+        return false;
+    }
+}
+
+// Optimal solution
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int low = 0;
+        int high = m * n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            int i = mid / n;
+            int j = mid % n;
+            if(matrix[i][j] == target){
+                return true;
+            } else if(matrix[i][j] > target){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
         return false;
     }
