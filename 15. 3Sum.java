55b3d4c0e9a516d08ff85deed3faa90ff70dec63
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-20 21:29:10 +0530

    Update 15. 3Sum.java

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -24,3 +24,32 @@ class Solution {
         return list;
     }
 }
+
+// Better approach using set
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        Set<List<Integer>> s = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            Set<Integer> set = new HashSet<>();
+            for(int j = i + 1; j < n; j++){
+                int sum = -1 * (nums[i] + nums[j]);
+                if(set.contains(sum)){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(sum);
+                    temp.add(nums[j]);
+                    if(!s.contains(temp)){
+                        s.add(temp);
+                        list.add(temp);
+                    }
+                }
+                set.add(nums[j]);
+            }
+        }
+        return list;
+    }
+}
