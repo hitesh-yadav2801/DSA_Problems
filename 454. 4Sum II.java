cbb5758614966b3f1e3a0ed869fa9c25d34545e0
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-21 00:15:30 +0530

    Create 454. 4Sum II.java

---
+++ 454. 4Sum II.java
@@ -0,0 +1,35 @@
+// Brute force O(n^4)
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int count = 0;
+        for(int i : nums1)
+            for(int j : nums2)
+                for(int k : nums3)
+                    for(int l : nums4)
+                        if(i + j + k + l == 0) count++;
+        return count;
+    }
+}
+
+// Slightly better approach but still TLE
+
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums1){
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        int count = 0;
+        for(int i : nums2){
+            for(int j : nums3){
+                for(int k : nums4){
+                    if(map.containsKey(-(i + j + k))){
+                        count += map.get(-(i + j + k));
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
