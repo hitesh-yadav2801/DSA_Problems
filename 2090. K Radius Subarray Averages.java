8a03da9105d70eed5398f6849563eb79d62a41ee
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-21 01:28:30 +0530

    Update 2090. K Radius Subarray Averages.java

--- 2090. K Radius Subarray Averages.java
+++ 2090. K Radius Subarray Averages.java
@@ -20,3 +20,34 @@ class Solution {
         return ans;
     }
 }
+
+// Prefix sum solution
+
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        if(k == 0) return nums;
+        int len = 2 * k + 1;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        if(n < len){
+            return ans;
+        }
+        long[] prefixSum = new long[n];
+        prefixSum[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+        for(int i = k; i < n - k; i++){
+            int left = i - k;
+            int right = i + k;
+            long sum = prefixSum[right];
+            if(left > 0){
+                sum -= prefixSum[left - 1];
+            }
+            int avg = (int)sum / len;
+            ans[i] = avg;
+        }
+        return ans;
+    }
+}
