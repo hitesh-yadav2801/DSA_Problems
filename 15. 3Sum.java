c6b45fb5b9695140e0650237eeb3bc87eb0d139b
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-20 11:01:39 +0530

    Create 15. 3Sum.java

---
+++ 15. 3Sum.java
@@ -0,0 +1,26 @@
+// Brute force(TLE)
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(sum == 0){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        if(!list.contains(temp)){
+                            list.add(temp);
+                        }
+                        continue;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
