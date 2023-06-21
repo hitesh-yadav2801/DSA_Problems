4ad4968a6f3cb3d725400881b637d167e8745b07
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-21 22:32:59 +0530

    Update 2448. Minimum Cost to Make Array Equal.java

--- 2448. Minimum Cost to Make Array Equal.java
+++ 2448. Minimum Cost to Make Array Equal.java
@@ -15,3 +15,37 @@ class Solution {
         return minCost;
     }
 }
+
+// Binary Search
+
+class Solution {
+    public long minCost(int[] nums, int[] cost) {
+        int n = nums.length;
+        long minCost = Long.MAX_VALUE;
+        long low = Integer.MAX_VALUE;
+        long high = Integer.MAX_VALUE;
+        for(int i : nums){
+            low = Math.min(low, i);
+            high = Math.max(high, i);
+        }
+        while(low <= high){
+            long mid = low + (high - low) / 2;
+            long midCost = solve(nums, cost, mid);
+            long midRightCost = solve(nums, cost, mid + 1);
+            minCost = Math.min(midCost, midRightCost);
+            if(midCost < midRightCost){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return minCost;
+    }
+    public long solve(int[] nums, int[] cost, long x){
+        long tempCost = 0l;
+        for(int i = 0; i < nums.length; i++){
+            tempCost += (Math.abs(x - nums[i])) * cost[i];
+        }
+        return tempCost;
+    }
+}
