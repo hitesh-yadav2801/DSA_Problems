8f3568c591f3d04f65390179c10474d0aa4355d5
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-06-22 10:54:54 +0530

    Update 122. Best Time to Buy and Sell Stock II.java

--- 122. Best Time to Buy and Sell Stock II.java
+++ 122. Best Time to Buy and Sell Stock II.java
@@ -1,3 +1,43 @@
+// Recursion + Memoisation
+
+class Solution {
+    int n;
+    int[][] dp;
+
+    public int maxProfit(int[] prices) {
+        n = prices.length;
+        dp = new int[n][2];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return rec(prices, 0, true);
+    }
+
+    private int rec(int[] prices, int idx, boolean buy) {
+        if (idx == n)
+            return 0;
+
+        if (dp[idx][buy ? 1 : 0] != -1)
+            return dp[idx][buy ? 1 : 0];
+
+        int profit = 0;
+        if (buy) {
+            int bought = -prices[idx] + rec(prices, idx + 1, false);
+            int skip = rec(prices, idx + 1, true);
+            profit = Math.max(bought, skip);
+        } else {
+            int sold = prices[idx] + rec(prices, idx + 1, true);
+            int skip = rec(prices, idx + 1, false);
+            profit = Math.max(sold, skip);
+        }
+
+        dp[idx][buy ? 1 : 0] = profit; // Store the computed result
+
+        return profit;
+    }
+}
+
+
 // Optimal Approach
 
 class Solution {
