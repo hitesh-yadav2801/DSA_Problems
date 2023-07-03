7d49326371cbeb95646e2f7b3b19597dde699610
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-03 20:58:52 +0530

    Update Longest Subarray With Sum K.java

--- Longest Subarray With Sum K.java
+++ Longest Subarray With Sum K.java
@@ -19,3 +19,29 @@ public class Solution {
 		return ans;
 	}
 }
+
+// Better approach using mapping
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int getLongestSubarray(int []nums, int k) {
+		int prefixSum = 0;
+		int ans = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < nums.length; i++){
+			prefixSum += nums[i];
+			if(prefixSum == k){
+				ans = Math.max(ans, i + 1);
+			}
+			int rsum = prefixSum - k;
+			if(map.containsKey(rsum)){
+				ans = Math.max(ans, i - map.get(rsum));
+			}
+			if(!map.containsKey(prefixSum)){
+				map.put(prefixSum, i);
+			}
+		}
+		return ans;
+	}
+}
