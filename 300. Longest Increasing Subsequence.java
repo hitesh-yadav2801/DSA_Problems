4046f5407be657e902a3f3ff0d0f2cedea31aab0
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-22 00:44:25 +0530

    Update 300. Longest Increasing Subsequence.java

--- 300. Longest Increasing Subsequence.java
+++ 300. Longest Increasing Subsequence.java
@@ -1,3 +1,30 @@
+// Memo
+
+class Solution {
+    int[][] dp;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        dp = new int[n][n + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return rec(nums, 0, -1);
+    }
+    public int rec(int[] nums, int idx, int prevIdx){
+        if(idx == nums.length) return 0;
+        if(dp[idx][prevIdx + 1] != -1) return dp[idx][prevIdx + 1];
+        int take = 0;
+        if(prevIdx == -1 || nums[idx] > nums[prevIdx]){
+            take = 1 + rec(nums, idx + 1, idx);
+        }
+        int notTake = 0 + rec(nums, idx + 1, prevIdx);
+        
+        return dp[idx][prevIdx + 1] = Math.max(take, notTake);
+    }
+}
+
+// Tabulation
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
