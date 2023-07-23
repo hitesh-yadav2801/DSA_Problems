2c6703d3e2d21ecaea5bfb11de8f3b7189c65cb3
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-07-23 22:13:34 +0530

    Create Binary Numbers.java

---
+++ Binary Numbers.java
@@ -0,0 +1,15 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static int countSetBits(int n) {
+		int count = 0;
+		while(n > 0){
+			if((n & 1) == 1){
+				count++;
+			}
+			n = n >> 1;
+		}
+		return count;
+	}
+}
