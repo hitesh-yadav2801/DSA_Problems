dc055e66481564f2a8bfca2961eb2726de1f4480
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2023-08-20 20:09:21 +0530

    Update 47. Permutations II.java

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -24,3 +24,31 @@ class Solution {
         }
     }
 }
+
+//
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int[] vis = new int[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, new ArrayList<>(), vis);
+        return ans;
+    }
+    public void backtrack(int[] nums, List<List<Integer>> ans, List<Integer> temp, int[] vis){
+        if(temp.size() == nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(vis[i] == 1 || (i > 0 && nums[i] == nums[i - 1] && vis[i - 1] == 0)){
+                continue;
+            }
+            temp.add(nums[i]);
+            vis[i] = 1;
+            backtrack(nums, ans, temp, vis);
+            temp.remove(temp.size() - 1);
+            vis[i] = 0;
+        }
+    }
+}
+
