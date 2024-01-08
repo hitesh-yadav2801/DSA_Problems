04bd5cd5036d4702727e821c02a3f8005eb74998
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-01-09 02:07:42 +0530

    Update Range Sum of BST.java

--- Range Sum of BST.java
+++ Range Sum of BST.java
@@ -1,35 +1,3 @@
-// Using inOrder traversal
-
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        inOrder(root, list);
-        return list;
-    }
-    void inOrder(TreeNode root, ArrayList<Integer> list){
-        if(root == null) return;
-        inOrder(root.left, list);
-        list.add(root.val);
-        inOrder(root.right, list);
-    }
-}
-
 // Using simple recursion
 
 class Solution {
@@ -41,6 +9,27 @@ class Solution {
         }
         rangeSumBST(root.left, low, high);
         rangeSumBST(root.right, low, high);
+        return sum;
+    }
+}
+
+// Better solution 
+
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) return 0;
+
+        if(root.val >= low && root.val <= high){
+            sum += root.val;
+        }
+        if(root.val > low){
+            rangeSumBST(root.left, low, high);
+        }
+        if(root.val < high){
+            rangeSumBST(root.right, low, high);
+        }
+
         return sum;
     }
 }
