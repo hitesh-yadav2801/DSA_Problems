a415db8986b68dfd78da85ca36e6398275554d93
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-02-13 00:37:03 +0530

    Update 169. Majority Element.java

--- 169. Majority Element.java
+++ 169. Majority Element.java
@@ -16,3 +16,23 @@ class Solution {
         return 0;
     }
 }
+
+// o(n) time and O(1) space
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans = nums[0];
+        int freq = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(nums[i] == ans){
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return ans;
+    }
+}
