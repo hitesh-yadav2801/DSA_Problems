6353ae607ae420abe6718707969c338bb24ccdf5
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-05-01 20:55:26 +0530

    Update 1915. Number of Wonderful Substrings.java

--- 1915. Number of Wonderful Substrings.java
+++ 1915. Number of Wonderful Substrings.java
@@ -26,3 +26,30 @@ class Solution {
         return oddNums <= 1;
     }
 }
+
+
+// O(N * 10) Time and O(N) space
+
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        int n = word.length();
+        Map<Long, Long> map = new HashMap<>();
+        map.put(0l, 1l);
+        long curXor = 0;
+        long result = 0;
+        for(int i = 0; i < n; i++){
+            curXor ^= 1 << (word.charAt(i) - 'a');
+            if(map.containsKey(curXor)){
+                result += map.get(curXor);
+            }
+            for(int j = 0; j < 10; j++){
+                long tempXor = curXor ^ (1 << j);
+                if(map.containsKey(tempXor)){
+                    result += map.get(tempXor);
+                }
+            }
+            map.put(curXor, map.getOrDefault(curXor, 0l) + 1l);
+        }
+        return result;
+    }
+}
