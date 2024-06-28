7b30cbcc7f29ecae22514885b576ac5f40e291dc
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-06-28 11:58:36 +0530

    Update Detect cycle in a directed graph.java

--- Detect cycle in a directed graph.java
+++ Detect cycle in a directed graph.java
@@ -32,3 +32,42 @@ class Solution {
         return false;
     }
 }
+
+
+
+// Using BFS Kahn's Algo
+
+
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int n, ArrayList<ArrayList<Integer>> adj) {
+        int[] in = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            for(int child: adj.get(i)){
+                in[child]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(in[i] == 0) q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            count++;
+            
+            for(int child: adj.get(cur)){
+                in[child]--;
+                if(in[child] == 0){
+                    q.add(child);
+                }
+            }
+        }
+        
+        return count != n;
+    }
+}
