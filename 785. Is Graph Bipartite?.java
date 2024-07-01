60c45583c001ace420beafc4d6c3877ceab8ee01
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-07-01 23:02:39 +0530

    Update 785. Is Graph Bipartite?.java

--- 785. Is Graph Bipartite?.java
+++ 785. Is Graph Bipartite?.java
@@ -1,3 +1,4 @@
+// BFS
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
@@ -17,6 +18,33 @@ class Solution {
                     }
                     if(color[next] == color[cur]) return false;
                 }
+            }
+        }
+        return true;
+    }
+}
+
+// DFS
+
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+
+        for(int i = 0; i < n; i++){
+            if(color[i] == 0 && !dfs(graph, color, i, 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    boolean dfs(int[][] adj, int[] color, int node, int curColor){
+        color[node] = curColor;
+
+        for(int child: adj[node]){
+            if(color[child] == color[node]) return false;
+            if(color[child] == 0){
+                if(dfs(adj, color, child, -curColor) == false) return false;
             }
         }
         return true;
