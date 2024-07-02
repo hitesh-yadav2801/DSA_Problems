58b8d03a85ee579dc03b28128e9aa8222b10b0dc
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-07-02 22:46:13 +0530

    Update Detect cycle in an undirected graph.java

--- Detect cycle in an undirected graph.java
+++ Detect cycle in an undirected graph.java
@@ -130,3 +130,60 @@ class Solution {
         return false;
     }
 }
+
+
+// Union find(DSU)
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int n, ArrayList<ArrayList<Integer>> adj) {
+       int[] parent = new int[n];
+       int[] rank = new int[n];
+       Arrays.fill(rank, 0);
+       
+       for(int i = 0; i < n; i++){
+           parent[i] = i;
+       }
+       for(int i = 0; i < n; i++){
+           parent[i] = i;
+       }
+       
+       for(int u = 0; u < n; u++){
+           for(int v: adj.get(u)){
+               if(u < v){
+                   int parent_u = find(u, parent);
+                   int parent_v = find(v, parent);
+                   
+                   if(parent_u == parent_v) return true;
+                   
+                   union(u, v, parent, rank);
+               }
+           }
+       }
+       return false;
+       
+    }
+    int find(int i, int[] parent){
+        if(i == parent[i]){
+            return i;
+        }
+        
+        return parent[i] = find(parent[i], parent);
+    }
+    void union(int x, int y, int[] parent, int[] rank){
+        int x_parent = find(x, parent);
+        int y_parent = find(y, parent);
+        
+        if(x_parent == y_parent) return;
+        
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        } else if(rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+}
