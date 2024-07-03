b05e52e0f2b5cc5727911c43fc80c0323f511ebd
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-07-03 15:52:02 +0530

    Update Implementing Dijkstra Algorithm.java

--- Implementing Dijkstra Algorithm.java
+++ Implementing Dijkstra Algorithm.java
@@ -1,88 +1,38 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.io.*;
-import java.lang.*;
-
-class DriverClass
-{
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int V = Integer.parseInt(str[0]);
-            int E = Integer.parseInt(str[1]);
-    
-            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-            for(int i=0;i<V;i++)
-            {
-                adj.add(new ArrayList<ArrayList<Integer>>());
-            }
-            
-            int i=0;
-            while (i++<E) {
-                String S[] = read.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                int w = Integer.parseInt(S[2]);
-                ArrayList<Integer> t1 = new ArrayList<Integer>();
-                ArrayList<Integer> t2 = new ArrayList<Integer>();
-                t1.add(v);
-                t1.add(w);
-                t2.add(u);
-                t2.add(w);
-                adj.get(u).add(t1);
-                adj.get(v).add(t2);
-            }
-            
-            int S = Integer.parseInt(read.readLine());
-            
-            Solution ob = new Solution();
-            
-            int[] ptr = ob.dijkstra(V, adj, S);
-            
-            for(i=0; i<V; i++)
-                System.out.print(ptr[i] + " ");
-            System.out.println();
-        }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
-class Solution
-{
-    //Function to find the shortest distance of all the vertices
-    //from the source vertex S.
-    static int[] dijkstra(int v, ArrayList<ArrayList<ArrayList<Integer>>> adj, int source)
-    {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
-        int[] res = new int[v];
-        Arrays.fill(res, Integer.MAX_VALUE);
-        res[source] = 0;
-        pq.offer(new int[]{0, source});
+class Solution {
+    static int[] dijkstra(int n, ArrayList<ArrayList<ArrayList<Integer>>> adj, int src) {
+        int[] ans = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[src] = 0; 
         
-        while(!pq.isEmpty()){
-            int[] pair = pq.peek();
-            pq.poll();
-            int dist = pair[0];
-            int node = pair[1];
-            
-            for(int i = 0; i < adj.get(node).size(); i++){
-                int adjNode = adj.get(node).get(i).get(0);
-                int wt = adj.get(node).get(i).get(1);
-                if(dist + wt < res[adjNode]){
-                    res[adjNode] = dist + wt;
-                    pq.offer(new int[]{res[adjNode], adjNode});
+        boolean[] visited = new boolean[n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+
+        pq.offer(new int[]{src, 0});
+
+        while (!pq.isEmpty()) {
+            int[] pair = pq.poll();
+            int node = pair[0];
+            int dist = pair[1];
+
+
+            /// we can remove visited array if we want
+            /// visited array will optimize the solution
+            if (visited[node]) {
+                continue;
+            }
+
+            visited[node] = true;
+
+            for (ArrayList<Integer> child : adj.get(node)) {
+                int adjNode = child.get(0);
+                int wt = child.get(1);
+
+                if (!visited[adjNode] && dist + wt < ans[adjNode]) {
+                    ans[adjNode] = dist + wt;
+                    pq.add(new int[]{adjNode, ans[adjNode]});
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
-
