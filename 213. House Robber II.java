1c70c095d0e7e36dd6896ebb6fdbd2fdc646f250
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-07-04 20:50:34 +0530

    Update 213. House Robber II.java

--- 213. House Robber II.java
+++ 213. House Robber II.java
@@ -1,3 +1,4 @@
+// Recursion + memo
 class Solution {
     int[] dp;
     public int rob(int[] nums) {
@@ -21,5 +22,36 @@ class Solution {
         int not_rob = solve(nums, idx + 1, n);
 
         return dp[idx] = Math.max(rob, not_rob);
+    }
+}
+
+// Tabulation
+
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 2; i < n; i++){
+            int rob = dp[i - 2] + nums[i - 1];
+            int not_rob = dp[i - 1];
+            dp[i] = Math.max(rob, not_rob);
+        }
+        int take_0th = dp[n - 1];
+        Arrays.fill(dp, 0);
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++){
+            int rob = dp[i - 2] + nums[i - 1];
+            int not_rob = dp[i - 1];
+            dp[i] = Math.max(rob, not_rob);
+        }
+        int not_take_0th = dp[n];
+
+        return Math.max(take_0th, not_take_0th);
     }
 }
