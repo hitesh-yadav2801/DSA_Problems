58e78d3a3584a679018175fdc030c43bec788229
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2024-08-20 02:31:13 +0530

    Update 2316. Count Unreachable Pairs of Nodes in an Undirected Graph.java

--- 2316. Count Unreachable Pairs of Nodes in an Undirected Graph.java
+++ 2316. Count Unreachable Pairs of Nodes in an Undirected Graph.java
@@ -1,3 +1,5 @@
+// Using DFS
+
 class Solution {
     public long countPairs(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
@@ -32,5 +34,58 @@ class Solution {
             }
         }
         return size;
+    }
+}
+
+
+// By using DSU
+
+
+class Solution {
+    public long countPairs(int n, int[][] edges) {
+        long ans = 0l;
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+        Arrays.fill(rank, 0);
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            union(u, v, parent, rank);
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            int i_parent = find(i, parent);
+            map.put(i_parent, map.getOrDefault(i_parent, 0) + 1);
+        }
+        int remSize = n;
+        for(int key: map.keySet()){
+            int componentSize = map.get(key);
+            System.out.println(componentSize);
+            ans += (long)componentSize * (remSize - componentSize);
+            remSize -= componentSize;
+        }
+        return ans;
+    }
+    int find(int i, int[] parent){
+        if(parent[i] == i) return i;
+        return parent[i] = find(parent[i], parent);
+    }
+    void union(int i, int j, int[] parent, int[] rank){
+        int x_parent = find(i, parent);
+        int y_parent = find(j, parent);
+        if(x_parent == y_parent) return;
+        
+        if(rank[x_parent] < rank[y_parent]){
+            parent[x_parent] = y_parent;
+        } else if(rank[y_parent] < rank[x_parent]){
+            parent[y_parent] = x_parent;
+        } else {
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
     }
 }
