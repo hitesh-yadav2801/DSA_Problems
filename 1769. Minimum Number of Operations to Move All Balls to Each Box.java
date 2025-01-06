3815796bb9c33a0588ccab956c4c44c58e05ce6d
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2025-01-07 00:57:46 +0530

    Create 1769. Minimum Number of Operations to Move All Balls to Each Box.java

---
+++ 1769. Minimum Number of Operations to Move All Balls to Each Box.java
@@ -0,0 +1,16 @@
+// Brute force
+
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++){
+            if(boxes.charAt(i) == '1'){
+                for(int j = 0; j < n; j++){
+                    ans[j] += Math.abs(i - j);
+                }
+            }
+        }
+        return ans;
+    }
+}
