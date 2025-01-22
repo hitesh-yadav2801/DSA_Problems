1ee045d89b292da8a5db2e4265b85fabf07321d7
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2025-01-22 23:41:17 +0530

    Update Intersection of Two Arrays II.java

--- Intersection of Two Arrays II.java
+++ Intersection of Two Arrays II.java
@@ -77,20 +77,14 @@ class Solution {
 // Optimal solution O(NlogN) TC:
 
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         ArrayList<Integer> list = new ArrayList<>();
         int i = 0, j = 0;
         while(i < nums1.length && j < nums2.length){
             if(nums1[i] == nums2[j]){
-                if(list.size() != 0){
-                    if(list.get(list.size() - 1) != nums1[i]){
-                        list.add(nums1[i]);
-                    }
-                } else {
-                    list.add(nums1[i]);
-                }
+                list.add(nums1[i]);
                 i++;
                 j++;
             } else if(nums1[i] < nums2[j]){
