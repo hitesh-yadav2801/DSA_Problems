c81b02ea8189ec599d8a47322984071b757d8657
Hitesh Kumar Yadav <92979306+hitesh-yadav2801@users.noreply.github.com>
2025-02-03 22:24:34 +0530

    Update 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.java

--- 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.java
+++ 3105. Longest Strictly Increasing or Strictly Decreasing Subarray.java
@@ -27,3 +27,29 @@ class Solution {
         return max;
     }
 }
+
+
+// Optimal Apporoach O(n)
+
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int incLen = 1, decLen = 1;
+        int n = nums.length;
+        int max = 1;
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[i-1]){
+                incLen++;
+                decLen = 1;
+            } else if(nums[i] < nums[i-1]){
+                decLen++;
+                incLen = 1;
+            } else {
+                incLen = 1;
+                decLen = 1;
+            }
+            max = Math.max(max, Math.max(incLen, decLen));
+        }
+        return max;
+    }
+}
